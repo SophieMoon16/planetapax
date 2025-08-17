1b774b37127cf4f99543d4446d61b84d1b2794cc
SophieMoon16 <sluu160593@gmail.com>
2025-08-17 17:46:03 +0200

    add two more pages and modifications

--- app.js
+++ app.js
@@ -3,26 +3,44 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Définir EJS comme moteur de templates
+// Define the view engine and views directory
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Servir les fichiers statiques (CSS, images...)
+// Serve static files from the resources directory
 app.use(express.static(path.join(__dirname, "resources")));
 
-// Route principale (page d'accueil)
+// ROUTES
+// Main route for home page
 app.get("/", (req, res) => {
-  res.render("index", { title: "Bienvenue chez Planetapax !" });
+  res.render("index", {
+    title: "Bienvenue chez Planetapax !",
+  });
 });
 
-// Lancement de l'app
+// Route for the manifesto page
+app.get("/manifesto", (req, res) => {
+  res.render("manifest", {
+    title: "Notre Manifesto",
+  });
+});
+
+// Route for faq page
+app.get("/faq", (req, res) => {
+  res.render("faq", {
+    title: "Foire Aux Questions",
+  });
+});
+
+// Launching app
 app.listen(PORT, () => {
   console.log("Application Node.js en écoute sur le port " + PORT);
 });
 
+// Detecting errors
 app.use((err, req, res, next) => {
-  console.error("Erreur attrapée :", err.stack);
-  res.status(500).send(`<pre>Erreur serveur :\n${err.stack}</pre>`);
+  console.error("Catched error :", err.stack);
+  res.status(500).send(`<pre>Server error:\n${err.stack}</pre>`);
 });
 
 module.exports = app;
--- resources/js/index.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-  document.getElementById("unique-sidenav").style.width = "200px";
-}
-
-function closeNav() {
-  document.getElementById("unique-sidenav").style.width = "0";
-}
